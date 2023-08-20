80c485723ccb9364d751c936ac725f3d26cec1cf
Rafael Gomez <rgomez2014@gmail.com>
2023-08-20 17:56:37 -0500

    validated text length, shape color, text color

---
+++ utils/validate.js
@@ -0,0 +1,23 @@
+const chroma = require('chroma.js')
+function textValidate(text) {
+    if (text.length > 3){
+        return "Text must be maximum of 3 characters long.";
+    }
+    return true;
+}
+
+function textColorValidate(textColor){
+    if (!chroma.valid(textColor)){
+        return "Please enter a valid color name or hexadecimal number";
+    }
+    return true;
+}
+
+function shapeColorValidate(shapeColor){
+    if (!chroma.valid(shapeColor)){
+    return "Please enter a valid color name or hexadecimal number";
+    }
+    return true;
+}
+
+module.exports = {textValidate, textColorValidate, shapeColorValidate}
