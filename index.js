911558a47a0e6fbdb3021e00a6ed00c76d0f82bf
Rafael Gomez <rgomez2014@gmail.com>
2023-08-20 18:47:29 -0500

    Formated and shape set

--- index.js
+++ index.js
@@ -2,36 +2,57 @@ const inquirer = require('inquirer')
 
 const fs = require('fs')
 
-const {textValidate, textColorValidate, shapeColorValidate} = require('./utils/validate')
-
+const { textValidate, textColorValidate, shapeColorValidate } = require('./utils/validate')
+const { Circle, Square, Triangle} = require('./lib/shapes/shape')
 
 const questions = [
     {
-        name:'text',
-        type:'input',
-        message:'Please enter at most three characters, for exmaple SVG, AOL, SPY ',
+        name: 'text',
+        type: 'input',
+        message: 'Please enter at most three characters, for exmaple SVG, AOL, SPY ',
         validate: textValidate,
     },
     {
-        name:'textColor',
-        type:'input',
-        message:'Please enter a color name (like "blue") or hexadecimal number  (like "#FF0000") for the text color',
+        name: 'textColor',
+        type: 'input',
+        message: 'Please enter a color name (like "blue") or hexadecimal number  (like "#FF0000") for the text color',
         validate: textColorValidate
     },
     {
         name: 'shape',
-        type:'list',
-        message:'Please choose from one of the shapes presented',
+        type: 'list',
+        message: 'Please choose from one of the shapes presented',
         choices: ["Circle", "Triangle", "Square"]
     },
     {
         name: 'shapeColor',
-        type:'input',
-        message:'Please enter a color name (like "blue") or hexadecimal number  (like "#FF0000") for the shape color',
+        type: 'input',
+        message: 'Please enter a color name (like "blue") or hexadecimal number  (like "#FF0000") for the shape color',
         validate: shapeColorValidate
     }
 ]
 
-inquirer.createPromptModule(questions).then((answers) =>{
-    
+function writeTofile(fileName, answers) {
+    const {
+        text,
+        textColor,
+        shape,
+        shapeColor
+    } = answers
+}
+
+inquirer.prompt(questions).then((answers) => {
+    let shape;
+    switch (answers.shape) {
+        case 'Circle':
+            shape = new Circle()
+            break;
+        case 'Square':
+            shape = new Square()
+            break;
+        case 'Triangle':
+            shape = new Triangle()
+            break;
+    }
+
 })
