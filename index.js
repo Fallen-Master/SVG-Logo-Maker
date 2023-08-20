1062524f806f5a366aeb444500e7b27313b0ed58
Rafael Gomez <rgomez2014@gmail.com>
2023-08-20 16:36:16 -0500

    added questions and set up index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const inquire = 
+const inquirer = require('inquirer')
+
+const fs = require('fs')
+
+
+const quiestions = [
+    {
+        name:'text',
+        type:'input',
+        message:'Please enter at most three characters, for exmaple SVG, AOL, SPY '
+    },
+    {
+        name:'textColor',
+        type:'input',
+        message:'Please enter a color name (like "blue") or hexadecimal number  (like "#FF0000") for the text color'
+    },
+    {
+        name: 'shape',
+        type:'list',
+        message:'Please choose from one of the shapes presented',
+        choices: ["Circle", "Triangle", "Square"]
+    },
+    {
+        name: 'shape color',
+        type:'input',
+        message:'Please enter a color name or hexadecimal number for the shape color'
+    }
+]
+
+function init()
